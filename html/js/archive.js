786fe0400a2bcd9311d68c2786217780c1ef78fe
css3studio <css3studio@naver.com>
2017-10-30 10:01:12 +0800

    메인 및 뉴스리스트 1착 작업분
    메인 및 뉴스리스트 1차 작업분입니다.

---
+++ html/js/archive.js
@@ -0,0 +1,105 @@
+/*
+Theme Name: Seoul Digital Archive
+Author: css3studio
+Version:2.0
+*/
+var $ = jQuery;
+var header_height;
+var fontSize;
+
+$(window).resize(function() {
+	header_height = $("header").height();
+	fontSize = parseFloat($("html").css("fontSize"));
+});
+
+$(window).scroll(function(e){
+	//메인화면 스크롤링
+	if ($("#main_wrap").hasClass("mainPage")){	//main
+		if ($(window).scrollTop() > header_height){
+			$("header").removeClass("hd01");
+			$("header").addClass("hd02");
+			$("#main_wrap").addClass("scrolling");
+			$("#main-content").css("paddingTop",header_height + (fontSize * 6));
+		} else{
+			$("header").removeClass("hd02");
+			$("header").addClass("hd01");
+			$("#main_wrap").removeClass("scrolling");
+			$("#main-content").css("paddingTop","3rem");
+		}
+	} else {		//sub
+		if ($(window).scrollTop() > 0){
+			$("#main_wrap").addClass("scrolling");
+			$("#main-content").css("paddingTop",header_height);
+		} else{
+			$("#main_wrap").removeClass("scrolling");
+			$("#main-content").css("paddingTop",0);
+		}
+	}
+});
+
+$(document).ready(function() {
+	header_height = $("header").height();
+	fontSize = parseFloat($("html").css("fontSize"));
+	//폼요소 스타일링
+	$(".cf02 select").selectmenu({
+		change: function( event, data ) {
+			if(data.item.value == "0")
+				$(".cf02 button").text("웹사이트에서 검색");
+			else
+				$(".cf02 button").text("기록에서 검색");
+		}
+	});
+	$(".tkg01").buttonset();
+	//검색창 제어
+	$("header menu li.search a").click(function(){
+		$("header").addClass("searchMode");
+		return false;
+	});
+	$("header menu li a:not(li.search a)").click(function(){
+		$("header").removeClass("searchMode");
+		return false;
+	});
+	$(".cf02 .closeBtn").click(function(){
+		$("header").removeClass("searchMode");
+	});
+	
+	//관리자 메뉴
+	$(".ng05>li>a").click(function(){
+		$(this).parent().siblings().removeClass("on");
+		$(this).parent().addClass("on");
+		$(".fc05").removeClass("viewMode");
+		return false;
+	});
+	$(".ng05>li>dl li a").click(function(){
+		$(this).parent().siblings().removeClass("on");
+		$(this).parent().addClass("on");
+		if(!$(".fc05").hasClass("viewMode"))	$(".fc05").addClass("viewMode");
+		return false;
+	});
+	
+	
+	
+	
+	
+	/*
+	$(".ng04 select").selectmenu();
+
+	$(".tc02 dd a").mouseenter(function(){
+		$(this).next().fadeIn();
+	});
+	$(".tc02 dd a").mouseleave(function(){
+		$(this).next().fadeOut();
+	});
+	//열람하기
+	$(".ng08 li.open a").click(function(){
+		$(".etc01").slideDown();
+		return false;
+	});
+	$(".etc01 a.close").click(function(){
+		$(".etc01").slideUp();
+		return false;
+	});
+	*/
+});
+
+
